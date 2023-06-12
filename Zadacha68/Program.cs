fc43e2f7a137a9e0158e2f13a56e1d0c4d164b79
woynaw <woynaw@yandex.ru>
2023-06-12 19:38:09 +0300

    add Zadacha68

--- Zadacha68/Program.cs
+++ Zadacha68/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+int Akkerman(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if (n == 0) return Akkerman(m - 1, 1);
+    else return Akkerman(m - 1, Akkerman(m, n - 1));
+}
+
+Console.WriteLine("Введите M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int result = Akkerman(m, n);
+Console.Write($"Функция Аккермана = {result} ");
