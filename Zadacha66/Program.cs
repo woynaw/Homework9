32cdb5e2bb993a5964cc3c23028d92d349c44514
woynaw <woynaw@yandex.ru>
2023-06-12 19:35:10 +0300

    add Zadacha66

--- Zadacha66/Program.cs
+++ Zadacha66/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int m = InputNumbers("Введите M: ");
+int n = InputNumbers("Введите N: ");
+int temp = m;
+
+if (m > n)
+{
+    m = n;
+    n = temp;
+}
+
+PrintSumm(m, n, temp = 0);
+
+void PrintSumm(int m, int n, int summ)
+{
+    summ = summ + n;
+    if (n <= m)
+    {
+        Console.Write($"Сумма элементов = {summ} ");
+        return;
+    }
+    PrintSumm(m, n - 1, summ);
+}
+
+int InputNumbers(string input)
+{
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
