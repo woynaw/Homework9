53f9ed1e758a894abbe2367653a4f5ee8944fc34
woynaw <woynaw@yandex.ru>
2023-06-12 19:30:24 +0300

    add Zadacha64

--- Zadacha64/Program.cs
+++ Zadacha64/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите N");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+void Counter(int number)
+{
+    if (number < 0) Console.Write($"{number} не натуральное число");
+    if (number == 0) return;
+    Console.Write($"{number} ");
+    Counter (number - 1);
+}
+Console.Write($"N = {number} -> ");
+Counter(number);
